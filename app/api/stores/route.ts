23f603de5ff30e6154f55109954209d767c415ff
aryantayal <atayal99@gmail.com>
2023-11-19 17:04:23 -0600

    fix cases smh

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -1,31 +1,36 @@
-import prismadb from "@/lib/prisma.ds"
+import { NextResponse } from 'next/server';
+import { auth } from '@clerk/nextjs';
+import prismadb from '@/lib/prisma.ds';
 
-import { auth } from "@clerk/nextjs"
-import { NextResponse } from "next/server"
 
-export async function POST(req:Request) {
-    try{
-        const {userId} = auth()
-        const body = await req.json()
 
-        const {name} = body;
+export async function POST(
+  req: Request,
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
 
-        if(!userId){
-            return new NextResponse("Unauthorized", {status: 401})
-        }
-        if(!name){
-            return new NextResponse("Name is required", {status: 400})
-        }
-        const store = await prismadb.store.create({
-            data:{
-                name,
-                userId
-            }
-        });
-        return NextResponse.json(store)
+    const { name } = body;
 
-    } catch(error){
-        console.log('[STORES_POST]', error)
-        return new NextResponse("Internal error", {status: 500})
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 403 });
     }
-}
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    const store = await prismadb.store.create({
+      data: {
+        name,
+        userId,
+      }
+    });
+  
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log('[STORES_POST]', error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+};
