c30d19ac527dc505a91ee2684837d039471b8bff
aryantayal <atayal99@gmail.com>
2023-11-21 21:29:06 -0600

    billboard creating fucntionality done

--- app/(dashboard)/[storeid]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeid]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { AlertModal } from "@/components/modals/alerts-modal";
-import { ApiAlert } from "@/components/ui/api-alert";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -15,7 +14,6 @@ import { Heading } from "@/components/ui/heading";
 import ImageUpload from "@/components/ui/image-upload";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
-import { useOrigin } from "@/hooks/use-origin";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Billboard } from "@prisma/client";
 import axios from "axios";
@@ -43,7 +41,6 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
 }) => {
   const params = useParams();
   const router = useRouter();
-  const origin = useOrigin();
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
--- app/(dashboard)/[storeid]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeid]/(routes)/billboards/components/client.tsx
@@ -3,11 +3,16 @@
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
+import { Billboard } from "@prisma/client";
 
 import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 
-export const BillboardClient = () => {
+interface BillboardClientProps {
+  data: Billboard[];
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
   const router = useRouter();
   const params = useParams();
 
@@ -15,7 +20,7 @@ export const BillboardClient = () => {
     <>
       <div className="flex items-center justify-between ">
         <Heading
-          title="Billboards (0)"
+          title={`Billboards (${data.length})`}
           description="Manage billboards for your store here
           "
         />
