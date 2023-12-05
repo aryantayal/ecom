190daa7e44ce6c0df9e6ed5595681d1cfdb352d6
aryantayal <atayal99@gmail.com>
2023-12-04 22:26:09 -0600

    adding size fix category and test still

--- app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/page.tsx
@@ -1,23 +1,20 @@
 import prismadb from "@/lib/prisma.ds";
-import { BillboardForm } from "./components/size-form";
+import { SizeForm } from "./components/size-form";
 
-const BillboardsPage = async ({
-  params,
-}: {
-  params: { billboardId: string };
-}) => {
-  const billboard = await prismadb.billboard.findUnique({
+const SizePage = async ({ params }: { params: { sizeId: string } }) => {
+  const size = await prismadb.size.findUnique({
     where: {
-      id: params.billboardId,
+      id: params.sizeId,
     },
   });
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardForm initialData={billboard} />
+        <SizeForm initialData={size} />
       </div>
     </div>
   );
 };
 
-export default BillboardsPage;
+export default SizePage;
